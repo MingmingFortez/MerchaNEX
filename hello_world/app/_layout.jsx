6d0a817dff9a5974f87b1163018e7308fad1da3f
Cassie Hopkin <cassie.hopkin13@gmail.com>
2024-06-26 14:30:29 +0800

    got rid of error for now

--- hello_world/app/_layout.jsx
+++ hello_world/app/_layout.jsx
@@ -1,28 +1,33 @@
 import { StyleSheet, Text, View } from 'react-native'
-import { SplashScreen, Stack } from 'expo-router';
-import { useFonts } from 'expo-font'
-import { useEffect } from 'react'
+import { Slot, Stack } from 'expo-router';
+//import { useFonts } from 'expo-font'
+//import { useEffect } from 'react'
 
 
-SplashScreen.preventAutoHideAsync();
+//SplashScreen.preventAutoHideAsync();
 
 
 const RootLayout = () => {
-  const [fontsLoaded, error] = useFonts({
-    "SpaceMono-Regular.ttf": require("../assets/fonts/SpaceMono-Regular.ttf"),
-  })
+  // const [fontsLoaded, error] = useFonts({
+  //   "SpaceMono-Regular.ttf": require("../assets/fonts/SpaceMono-Regular.ttf"),
+  // })
 
   // what to do when screen is loading
-  useEffect(() => {
-    if(error) throw error;
-    if(fontsLoaded) SplashScreen.hideAsync();
-    if (!fontsLoaded && !error) return null;
-  }, [fontsLoaded, error])
+  // useEffect(() => {
+  //   if(error) throw error;
+  //   if(fontsLoaded) SplashScreen.hideAsync();
+  //   if (!fontsLoaded && !error) return null;
+  // }, [fontsLoaded, error])
+  // return (
+  //   <Stack>
+  //       <Stack.Screen name="index" options={{ headerShown: false}}/>
+  //   </Stack>
+  // )
   return (
     <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false}}/>
+      <Stack.Screen name="index" option={ { headerShown: false} }/>
     </Stack>
-  )
+  );
 }
 
 export default RootLayout
