d1cbf96d9b2b7b23195791ba367c9f0e43d9691a
Cassie Hopkin <cassie.hopkin13@gmail.com>
2024-06-27 09:49:59 +0800

    got fonts workinggit add -A

--- hello_world/app/_layout.jsx
+++ hello_world/app/_layout.jsx
@@ -1,33 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native'
-import { Slot, Stack } from 'expo-router';
-//import { useFonts } from 'expo-font'
-//import { useEffect } from 'react'
+import { SplashScreen, Stack } from 'expo-router';
+import { useFonts } from 'expo-font'
 
 
-//SplashScreen.preventAutoHideAsync();
 
 
 const RootLayout = () => {
-  // const [fontsLoaded, error] = useFonts({
-  //   "SpaceMono-Regular.ttf": require("../assets/fonts/SpaceMono-Regular.ttf"),
-  // })
+  const [fontsLoaded, error] = useFonts({
+    "Inter-Black": require("../assets/fonts/Inter-Black.ttf"),
+  })
 
-  // what to do when screen is loading
-  // useEffect(() => {
-  //   if(error) throw error;
-  //   if(fontsLoaded) SplashScreen.hideAsync();
-  //   if (!fontsLoaded && !error) return null;
-  // }, [fontsLoaded, error])
-  // return (
-  //   <Stack>
-  //       <Stack.Screen name="index" options={{ headerShown: false}}/>
-  //   </Stack>
-  // )
   return (
     <Stack>
-      <Stack.Screen name="index" option={ { headerShown: false} }/>
+        <Stack.Screen name="index" options={{ headerShown: false}}/>
     </Stack>
-  );
+  )
 }
 
 export default RootLayout
---
+++ hello_world/app/index.jsx
@@ -0,0 +1,38 @@
+import { useCallback } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Link } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'Inter-Black': require('../assets/fonts/Inter-Black.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{ fontFamily: 'Inter-Black', fontSize: 30 }}>Hello World</Text>
+      <Link href="/profile" style={{ color: 'blue' }}>Go to Profile</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
