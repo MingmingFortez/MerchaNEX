6d9d696bdd61abd1f70de3cf05a6b05c8275eafd
Jasmine Fortez <jfortez@ucsc.edu>
2024-06-28 14:11:19 +0800

    lol scroll view?

--- hello_world/app/(tabs)/activity.jsx
+++ hello_world/app/(tabs)/activity.jsx
@@ -1,12 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { ScrollView, View, Text } from 'react-native';
+import React from 'react';
 
 const Activity = () => {
   return (
-    <View>
-      <Text>Activity</Text>
-    </View>
-  )
+    <ScrollView>
+      <View>
+        <Text>Activity</Text>
+        
+        <Text>More content 1</Text>
+        <Text>More content 2</Text>
+        <Text>More content 3</Text>
+        <Text>More content 4</Text>
+        <Text>More content 5</Text>
+        <Text>More content 6</Text>
+        <Text>More content 7</Text>
+        <Text>More content 8</Text>
+        <Text>More content 9</Text>
+        <Text>More content 10</Text>
+        <Text>More content 11</Text>
+        <Text>More content 12</Text>
+        <Text>More content 13</Text>
+        <Text>More content 14</Text>
+        <Text>More content 15</Text>
+        <Text>More content 16</Text>
+        <Text>More content 17</Text>
+        <Text>More content 18</Text>
+        <Text>More content 19</Text>
+        <Text>More content 20</Text>
+      </View>
+    </ScrollView>
+  );
 }
 
-export default Activity
+export default Activity;
