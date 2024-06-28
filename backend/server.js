3012fca7563ccc845428a22cc5f1a6d7fa67c709
Min Yi Jiang <112724206+yimijj@users.noreply.github.com>
2024-06-28 11:53:37 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 // server.js
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5432;
 
 
 app.get('/', (req, res) => {
