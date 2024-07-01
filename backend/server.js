033eda3dec5914954ca7de0c1f113c17c13c22c3
yimijj <112724206+yimijj@users.noreply.github.com>
2024-07-01 10:37:29 +0800

    updating client information

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,10 @@
 import pg from 'pg'
 const {Client} = pg
 const client = new Client({
-    user: 'myuser',
-    host: 'localhost',
-    database: 'mydatabase',
-    password: 'mypassword',
+    user: 'u8eniq7su6io8s',
+    host: 'ccba8a0vn4fb2p.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
+    database: 'd21orf41k6c3r2',
+    password: 'p54a40fc4963d81bc289f08a47008bd4fffb57304edf02a3e85563d30f6b35547',
     port: 5432,
 });
 client.connect();
