eeb7a42c11244d42a9b6eb860055fb8520a9b862
Taha <taha.zeeshan.11@gmail.com>
2024-07-01 10:55:29 +0800

    fixed server and database files

--- backend/server.js
+++ backend/server.js
@@ -8,21 +8,6 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./database");
 
-
-// server.js
-import pg from 'pg'
-const {Client} = pg
-const client = new Client({
-    user: 'u8eniq7su6io8s',
-    host: 'ccba8a0vn4fb2p.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
-    database: 'd21orf41k6c3r2',
-    password: 'p54a40fc4963d81bc289f08a47008bd4fffb57304edf02a3e85563d30f6b35547',
-    port: 5432,
-});
-client.connect();
-
-const express = require('express');
->>>>>>> 033eda3dec5914954ca7de0c1f113c17c13c22c3
 const app = express();
 
 app.use(express.json());
